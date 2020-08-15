18f97d84c06e4748a8fd8ae020ba4723e995ca82
cornibulus <cornibulus@gmail.com>
2020-08-15 17:17:25 -0400

    menu

---
+++ Assets/Scripts/State Machine/State Components/LoadSceneStateComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneStateComponent : StateComponent
+{
+    [Header("Will reload current scene if empty")]
+    public string sceneName;
+
+    public override void Enter()
+    {
+        if(this.sceneName == null || this.sceneName == "")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            return;
+        }
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public override StateComponentType GetStateComponentType()
+    {
+        return StateComponentType.NO_EXECUTE;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Hooks
  https://itch.io/jam/miz-jam-1
 
-Elapsed time: 4:23:41
+Elapsed time: 4:51:26
