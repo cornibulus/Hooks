0efa604caf3a14fb738effdba1ceb3f58a61ae48
cornibulus <cornibulus@gmail.com>
2020-08-15 11:48:57 -0400

    prefabs and some basic states

---
+++ Assets/Scripts/NoDraw.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class NoDraw : MonoBehaviour
+{ 
+    private void Awake()
+    { 
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/State Machine/State Components/DrawStateComponent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawStateComponent : StateComponent
+{
+    public SpriteRenderer sprite;
+
+    public override StateComponentType GetStateComponentType()
+    {
+        return StateComponentType.NO_EXECUTE;
+    }
+
+    public override void Enter()
+    {
+        if (sprite != null)
+            sprite.enabled = true;
+    }
+
+    public override void Exit()
+    {
+        if (sprite != null)
+            sprite.enabled = false;
+    }
+}
