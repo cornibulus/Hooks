6f15c0d48cfb371dc30dcd3d1bc33664d9ba39a4
cornibulus <cornibulus@gmail.com>
2020-08-15 18:05:32 -0400

    Tutorial2

---
+++ Assets/Scripts/State Machine/State Components/LoadNextSceneStateComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextSceneStateComponent : StateComponent
+{
+    public override void Enter()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public override StateComponentType GetStateComponentType()
+    {
+        return StateComponentType.NO_EXECUTE;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Hooks
  https://itch.io/jam/miz-jam-1
 
-Elapsed time: 5:07:37
+Elapsed time: 5:38:20
+
+
+TODO for each scene
+ - Add to build
+ - Optionally override level or Load Next Scene
